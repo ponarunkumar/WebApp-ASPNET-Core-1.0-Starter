4e078aacd14aaf4692be09175102c698afce476d
Pon ArunKumar Ramalingam <ponarunkumar@gmail.com>
2016-10-20 14:25:01 -0400

    Web App Starter Kit using Kestrel

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace ConsoleApplication
+{
+
+   public class Startup
+        {
+            public void Configure(IApplicationBuilder app)
+            {
+                app.Run( context => 
+                    {
+                        return context.Response.WriteAsync("Hello DotNetCore on the web");
+                    }
+                );
+            }
+        }
+    public class Program
+    {
+
+        public static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                        .UseKestrel()
+                        .UseStartup<Startup>()
+                        .Build();
+
+            host.Run();
+
+
+            // Console.WriteLine("Hey, welcome to the dotnet world. Your Name?");
+            // var name = Console.ReadLine();
+            // Console.WriteLine($" Hey {name}, I hope your core journey takesoff to new heights!!!");
+        }
+    }
+}
